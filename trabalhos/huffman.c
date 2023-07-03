a18361e6803aa7d398e6a4ed692fa14770ffbc3d
ThiagoChafado <thiagochafado123@gmail.com>
2023-07-03 11:53:30 -0300

    update huffman.c

--- trabalhos/huffman.c
+++ trabalhos/huffman.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Estruturado nó
+//FIZ SEGUINDO ESSES PASSOS A RISCA
+/*Passos:
+– Contar a frequência de todos os caracteres no caractero de entrada;
+– Montar um floresta (várias árvores), onde cada árvore é unitária e
+contém, além do caractere em si, seu número de ocorrências no caractero;
+– Ordenar a lista de árvores em ordem crescente de frequência;
+– Unir todas as árvores até formar um única árvore:
+● Selecionar as duas árvores que possuem as menores frequências
+● Cria uma nova árvore combinando as duas árvores obtidas anteriormente
+– A frequência desta árvore será a soma das frequências das árvores que foram unidas
+● Realocar esta árvore no conjunto de árvores, mantendo a ordenação*/
+
+// Estruturado do Nó da árvore
 struct Node {
     char data;
     unsigned int frequencia;
@@ -10,8 +22,8 @@ struct Node {
     struct Node* right;
 };
 
-// Função para criar um novo nó da árvore de Huffman
-struct Node* createNode(char data, unsigned int frequencia) {
+// novo nó
+struct Node* criaNó(char data, unsigned int frequencia) {
     struct Node* node = (struct Node*)malloc(sizeof(struct Node));
     node->data = data;
     node->frequencia = frequencia;
@@ -20,132 +32,112 @@ struct Node* createNode(char data, unsigned int frequencia) {
     return node;
 }
 
-// Função para trocar a posição de dois nós na floresta
-void swapNodes(struct Node** a, struct Node** b) {
-    struct Node* temp = *a;
+// trocando a posicao dos nos
+void swapNó(struct Node** a, struct Node** b) {
+    struct Node* aux = *a;
     *a = *b;
-    *b = temp;
+    *b = aux;
 }
 
-// Função para ordenar a lista de árvores em ordem crescente de frequência
-void sortForest(struct Node** forest, int size) {
+// ordenando a floresta com bubble sort
+void ordenaFloresta(struct Node** floresta, int size) {
     int i, j;
     for (i = 0; i < size - 1; i++) {
         for (j = 0; j < size - i - 1; j++) {
-            if (forest[j]->frequencia > forest[j + 1]->frequencia) {
-                swapNodes(&forest[j], &forest[j + 1]);
+            if (floresta[j]->frequencia > floresta[j + 1]->frequencia) {
+                swapNó(&floresta[j], &floresta[j + 1]);
             }
         }
     }
 }
-
-// Função para unir duas árvores e criar uma nova árvore
-struct Node* mergeTrees(struct Node* left, struct Node* right) {
-    struct Node* newNode = createNode('\0', left->frequencia + right->frequencia);
-    newNode->left = left;
-    newNode->right = right;
-    return newNode;
+// merge das arvores
+struct Node* mergeArvores(struct Node* left, struct Node* right) {
+    struct Node* novo = criaNó('\0', left->frequencia + right->frequencia);
+    novo->left = left;
+    novo->right = right;
+    return novo;
 }
 
-// Função para construir a árvore de Huffman
-struct Node* buildHuffmanTree(char* text) {
+/// funcao principal de criacao da arvore
+struct Node* criacaoArvore(char* caracter) {
     int frequencies[256] = {0}; // Array para armazenar as frequências de cada caractere
     int i;
 
-    // Contar a frequência de cada caractere no texto
-    for (i = 0; text[i] != '\0'; i++) {
-        frequencies[(int)text[i]]++;
+    // Contar a frequência de cada caractere no caractero
+    for (i = 0; caracter[i] != '\0'; i++) {
+        frequencies[(int)caracter[i]]++;
     }
 
     // Montar a floresta inicial com árvores unitárias
-    struct Node** forest = (struct Node**)malloc(256 * sizeof(struct Node*));
-    int forestSize = 0;
+    struct Node** floresta = (struct Node**)malloc(256 * sizeof(struct Node*));
+    int tamanhoFloresta = 0;
     for (i = 0; i < 256; i++) {
         if (frequencies[i] > 0) {
-            forest[forestSize++] = createNode((char)i, frequencies[i]);
+            floresta[tamanhoFloresta++] = criaNó((char)i, frequencies[i]);
         }
     }
 
-    // Ordenar a lista de árvores em ordem crescente de frequência
-    sortForest(forest, forestSize);
+    
+    ordenaFloresta(floresta, tamanhoFloresta);
 
-    // Unir todas as árvores até formar uma única árvore
-    while (forestSize > 1) {
-        // Selecionar as duas árvores com as menores frequências
-        struct Node* smallest1 = forest[0];
-        struct Node* smallest2 = forest[1];
+    // unindo arvore
+    while (tamanhoFloresta > 1) {
+        
+        struct Node* menor1 = floresta[0];
+        struct Node* menor2 = floresta[1];
 
-        // Criar uma nova árvore combinando as duas árvores selecionadas
-        struct Node* mergedTree = mergeTrees(smallest1, smallest2);
+        
+        struct Node* arvoreMergeada = mergeArvores(menor1, menor2);
 
-        // Realocar a nova árvore no conjunto de árvores, mantendo a ordenação
-        forest[0] = mergedTree;
-        forest[1] = forest[forestSize - 1];
-        forestSize--;
+        // recolocar a arvore 
+        floresta[0] = arvoreMergeada;
+        floresta[1] = floresta[tamanhoFloresta - 1];
+        tamanhoFloresta--;
 
-        sortForest(forest, forestSize);
+        ordenaFloresta(floresta, tamanhoFloresta);
     }
 
-    // A árvore final é a raiz da árvore de Huffman
-    struct Node* huffmanTree = forest[0];
+    
+    struct Node* arvore = floresta[0];
 
-    // Liberar memória
-    free(forest);
+    
+    free(floresta);
 
-    return huffmanTree;
+    return arvore;
 }
 
-// Função para imprimir os códigos de Huffman
-void printHuffmanCodes(struct Node* root, int* code, int depth) {
-    if (root->left == NULL && root->right == NULL) {
+// impressao in order
+void imprimirHuffman(struct Node* raiz, int* codigo, int profundidade) {
+    if (raiz->left == NULL && raiz->right == NULL) {
         int i;
-        printf("%c: ", root->data);
-        for (i = 0; i < depth; i++) {
-            printf("%d", code[i]);
+        printf("%c: ", raiz->data);
+        for (i = 0; i < profundidade; i++) {
+            printf("%d", codigo[i]);
         }
         printf("\n");
     }
     else {
-        code[depth] = 0;
-        printHuffmanCodes(root->left, code, depth + 1);
+        codigo[profundidade] = 0;
+        imprimirHuffman(raiz->left, codigo, profundidade + 1);
 
-        code[depth] = 1;
-        printHuffmanCodes(root->right, code, depth + 1);
+        codigo[profundidade] = 1;
+        imprimirHuffman(raiz->right, codigo, profundidade + 1);
     }
 }
 
 
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Estrutura do Nó da árvore
-struct Node {
-    char data;
-    unsigned frequencia;
-    struct Node* left, * right;
-};
-
 
 int main() {
     char input[100];
-
     fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0';  // remove o caractere de nova linha
+    input[strcspn(input, "\n")] = '\0'; // remove o caractere de nova linha
+    struct Node* arvore = criacaoArvore(input);
 
-    return 0;
-}*/
-int main() {
-    char input[100];
-    fgets(input, sizeof(input), stdin);
-    input[strcspn(input, "\n")] = '\0';
-    struct Node* huffmanTree = buildHuffmanTree(input);
-
-    int code[256];
-    int depth = 0;
+    int codigo[256];
+    int profundidade = 0;
 
     
-    printHuffmanCodes(huffmanTree, code, depth);
+    imprimirHuffman(arvore, codigo, profundidade);
     
 
     return 0;
